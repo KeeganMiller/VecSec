fdf1e81d50ad34238d1b65ab93c1cb8d86492b26
KeeganMiller <Keeganmiller.dev@gmail.com>
2024-08-09 15:38:00 +1000

    Misc

--- core/resource_manager.py
+++ core/resource_manager.py
@@ -6,6 +6,19 @@ class ResourceManager:
     active_objects: list[GameObject] = []
 
     @staticmethod
+    def add_game_object(go: GameObject):
+        if go not in ResourceManager.active_objects and go not in ResourceManager.objects_to_start:
+            go._initialize()
+            ResourceManager.objects_to_start.append(go)
+
+    @staticmethod
+    def remove_game_object(go: GameObject):
+        if go in ResourceManager.objects_to_start:
+            ResourceManager.objects_to_start.remove(go)
+        elif go in ResourceManager.active_objects:
+            ResourceManager.active_objects.remove(go)
+
+    @staticmethod
     def _update(dt: float):
         for obj in ResourceManager.active_objects:
             if obj.is_active:
---
+++ test_go.py
@@ -0,0 +1,14 @@
+from pyray import *
+from core.game_object import GameObject
+
+class TestGo(GameObject):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+
+    def _update(self, dt: float) -> None:
+        super()._update(dt)
+        print("GO:", self.name)
+
+    def _draw(self) -> None:
+        super()._draw()
+        draw_text(f"GO: {self.name}", self.position.x, self.position.y, 32, BLACK)
